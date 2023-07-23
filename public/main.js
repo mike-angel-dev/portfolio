280813b2ef5b384cca1a7e9c5ef5a8ebb932759c
Miguel Angel <74116758+mike-angel-dev@users.noreply.github.com>
2023-07-23 14:48:53 -0600

    Add files via upload
    first commit

---
+++ public/main.js
@@ -0,0 +1,10 @@
+let whatsapp=document.getElementById("iconwhats"),
+email=document.getElementById("email");
+
+whatsapp.addEventListener("click",()=>{
+    alert("");
+})
+
+email.addEventListener("click",()=>{
+    alert("");
+})
